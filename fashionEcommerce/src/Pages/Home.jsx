fb3416391f3fe9e7fde26e09f8c8f6ff9b87e6a8
Sagar <sagarsurya45@gmail.com>
2025-05-06 19:40:10 +0530

    working on banner

--- fashionEcommerce/src/Pages/Home.jsx
+++ fashionEcommerce/src/Pages/Home.jsx
@@ -11,75 +11,120 @@ import  touser_pants from "../assets/touser_pants.jpg"
 import  shorts from "../assets/shorts.jpg"
 import  brands from "../assets/brands.jpg"
 import  newArrivals from "../assets/newArrivals.jpg"
+import  banner from "../assets/banner.jpg"
 
 
 function HomePage(){
 
     return(
         <HomeLayout>
-            <div className="flex gap-5 m-5 justify-center itmes-center p-2 ">
-                    <div>
+            <div className=" productCategory relative flex gap-5 m-5 justify-center itmes-center p-2 text-center hover:cursor-pointer">
+
+                    <div className=" p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={mensFashion}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="mensfashion" />
-                        <p>Mens Fashion</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Mens Fashion</p>
+
+                          {/* Dropdown */}
+                        <div className="absolute border border-red-800 border-2 w-120 left-1/2 -translate-x-1/2 top-full mt-2 bg-white shadow-md rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+                            <ul className="text-sm text-gray-700">
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Shirts</li>
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Pants</li>
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Shoes</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={womensFashion}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="womensfashion" />
-                        <p>Womens Fashion</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Womens Fashion</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={ethnicFashion}
-                        className="w-24 h-20 rounded-full "
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2 "
                          alt="ethnicFashion" />
-                        <p>ethnic Fashion</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">ethnic Fashion</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={tshirt}
-                        className="w-24 h-20 rounded-full "
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2 "
                          alt="t-shirt" />
-                        <p>T-shirt</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">T-shirt</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={jeans}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="jeans" />
-                        <p>Jeans</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Jeans</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={shirts}
-                        className="w-24 h-20 rounded-full "
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="shirts" />
-                        <p>Shirts</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Shirts</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={touser_pants}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="touser&pants" />
-                        <p>Touser & Pants</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Touser & Pants</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={shorts}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="shorts" />
-                        <p>Shorts</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Shorts</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={brands}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="Brands" />
-                        <p>Brands</p>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">Brands</p>
                     </div>
-                    <div>
+
+                    <div className="p-2 group transition-transform duration-300 hover:scale-105">
                         <img src={newArrivals}
-                        className="w-24 h-20 rounded-full"
+                        className="w-24 h-20 transition-all duration-300 ease-in-out rounded-full group-hover:scale-110 my-2"
                          alt="new Arrivals" />
-                        <p>New Arrivals</p>
-                    </div>
+                        <p className="text-sm tracking-wide font-light tracking-wide font-light transition-transform duration-300 group-hover:scale-105 group-hover:underline">New Arrivals</p>
+
+                        <div className="absolute border border-red-800 border-2 w-48 left-3/40 -translate-x-1/2 top-full mt-2 bg-white shadow-md rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10">
+                            <ul className="text-sm text-gray-700">
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Shirts</li>
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Pants</li>
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Shoes</li>
+                            </ul>
+                        </div>
+                    </div>     
                     
             </div>
+            {/* banner */}
+            <div className="banner">
+                <div className="relative m-auto border border-red-800 border-2">
+                    <img src={banner}
+                     alt="banner"
+                     className="w-[80%] m-auto" />
+
+                     <div className="absolute top-1/2 border border-red-800 border-2">
+                     <h1>GET EXTRA <br/> 10% OFF </h1>
+                     <span>ON SHOPPING FOR </span>
+                     </div>
+                    
+            
+                </div>
+                
+            </div>
+
         </HomeLayout>
     )
 }
